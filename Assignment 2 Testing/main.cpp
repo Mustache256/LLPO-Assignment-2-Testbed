5f921409785bd0248a8fd9863ea39cff26b48c4e
Mustache256 <71705907+Mustache256@users.noreply.github.com>
2023-11-22 22:44:35 +0000

    Commit for pull

--- Assignment 2 Testing/main.cpp
+++ Assignment 2 Testing/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <stdio.h>
 #include <sys/types.h>
-#include <unistd.h>
+//#include <unistd.h>
 
-void forkExample()
+/*void forkExample()
 {
 	if (fork() == 0)
 		printf("Child fork\n");
 	else
 		printf("Parent fork\n");
-}
+}*/
 
 int main()
 {
-	forkExample();
+	//forkExample();
+
+	char* args[] = { "./EXEC", NULL };
+
+	_wexecvp(args[0], args);
 	return 0;
 }
